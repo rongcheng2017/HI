e551856262ba6f6889f71d7a045f6d5b7ca25191
frc <rongcheng_feng@163.com>
2021-10-22 16:24:44 +0800

    release下通过Xlog将日志写入本地文件

--- app/src/main/java/com/rongcheng/hi/log/CrashReportingTree.kt
+++ app/src/main/java/com/rongcheng/hi/log/CrashReportingTree.kt
@@ -1,10 +1,10 @@
 package com.rongcheng.hi.log
 
 import android.app.Application
-import com.rongcheng.hilog.HiLog
 import com.rongcheng.hilog.timer.Timber
 import com.tencent.mars.xlog.Log
 import com.tencent.mars.xlog.Xlog
+import java.io.File
 
 /** A tree which logs important information for crash reporting.  */
 class CrashReportingTree(context: Application) : Timber.Tree() {
@@ -12,14 +12,29 @@ class CrashReportingTree(context: Application) : Timber.Tree() {
 //        val SDCARD: String = Environment.getExternalStorageDirectory().absolutePath
 //        val logPath: String = SDCARD + "/marssample/log"
 
+    private val defaultTag = "XLog"
+
     init {
+        android.util.Log.i(defaultTag, "crash init")
         System.loadLibrary("c++_shared")
         System.loadLibrary("marsxlog")
-        val cachePath = "${context.filesDir}/marssample/xlog"
-        val xlog = Xlog()
-        Log.setLogImp(xlog)
+        val crashPath = "${context.filesDir}/marssample/xlog"
+        val xLogDir = File(crashPath)
+        if (xLogDir.exists()) {
+            if (xLogDir.isDirectory) {
+                xLogDir.list()?.forEach {
+                    val fileName = "$crashPath/$it"
+                    android.util.Log.i("frc", "file path $fileName ")
+                    val file = File(fileName)
+                    val content = file.readText()
+                    android.util.Log.i("frc", content)
+                }
+            }
+            android.util.Log.i("frc", "xLog crash file create ok")
+        }
+        Log.setLogImp(Xlog())
         Log.setConsoleLogOpen(false)
-        Log.appenderOpen(Xlog.LEVEL_INFO, Xlog.AppednerModeAsync, "", cachePath, "hahh", 0);
+        Log.appenderOpen(Xlog.LEVEL_INFO, Xlog.AppednerModeAsync, "", crashPath, "hah", 0)
     }
 
     override fun isLoggable(tag: String?, priority: Int): Boolean {
@@ -28,16 +43,19 @@ class CrashReportingTree(context: Application) : Timber.Tree() {
 
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         if (t != null) {
-            if (priority == android.util.Log.ERROR) {
-                Log.e(tag ?: "Xlog", message)
-            } else if (priority == android.util.Log.WARN) {
-                Log.w(tag ?: "Xlog", message)
-            }else if (priority==android.util.Log.WARN){
-                Log.i(tag ?: "Xlog", message)
+            when (priority) {
+                android.util.Log.ERROR -> {
+                    Log.e(tag ?: defaultTag, "$message ${t.localizedMessage}")
+                }
+                android.util.Log.WARN -> {
+                    Log.w(tag ?: defaultTag, "$message ${t.localizedMessage}")
+                }
+                android.util.Log.INFO -> {
+                    Log.i(tag ?: defaultTag, "$message ${t.localizedMessage}")
+                }
             }
+        } else {
+            Log.i(tag ?: defaultTag, message)
         }
-//        if (priority == android.util.Log.INFO) {
-//            HiLog.it(tag ?: "HiLogRelease", message)
-//        }
     }
 }
